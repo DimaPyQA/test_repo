dddfaf882c60deed4010b006f4ba0d4708e04af4
DimaPyQA <dimanlitvinoff@gmail.com>
2024-07-26 01:31:29 +0300

    first test

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import requests
 url = 'https://dzen.ru/'
 
 response = requests.get(url)
-print(response.status_code)
+status_code = response.status_code
+print(status_code)
 
+if status_code == 200:
+    print("Test successfully passed")
+else: print("Failure")
